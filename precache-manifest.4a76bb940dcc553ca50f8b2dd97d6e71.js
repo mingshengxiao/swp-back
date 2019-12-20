dd77a6fc6c44d10ee7aee34222e2ee496b4080d9
mingshengxiao <13981799547@163.com>
2019-12-20 09:17:50 +0000

    Deploying to gh-pages from master 77ff727c51889c4fefb398d162b0848639f687f0

---
+++ precache-manifest.4a76bb940dcc553ca50f8b2dd97d6e71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95658ead1f43294947356ecde4a5d12f",
+    "url": "/swp-back/index.html"
+  },
+  {
+    "revision": "610347580ea1ce5454c2",
+    "url": "/swp-back/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a59c03d49c6621556cba",
+    "url": "/swp-back/static/js/2.9ddb8969.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/swp-back/static/js/2.9ddb8969.chunk.js.LICENSE"
+  },
+  {
+    "revision": "610347580ea1ce5454c2",
+    "url": "/swp-back/static/js/main.8640e90d.chunk.js"
+  },
+  {
+    "revision": "d18edcfddc3c0c7d4f22",
+    "url": "/swp-back/static/js/runtime-main.6b23da5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swp-back/static/media/logo.5d5d9eef.svg"
+  }
+]);
